b864cd5dd8fe825453f3b8f75af433608f72f8d3
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-10-05 10:42:01 +0300

    add model

--- CustomNavBar/Model/AppItem.swift
+++ CustomNavBar/Model/AppItem.swift
@@ -10,4 +10,24 @@ import SwiftUI
 
 struct AppItem: Identifiable {
     
-
+    var id = UUID().uuidString
+    // both Image And Name Are Same.. I.
+    var name: String
+    // since all Are Apple Native Apps.
+    var source: String = "Apple"
+    
+    var apps = [
+        AppItem(name: "appstore"),
+        AppItem(name: "calculator"),
+        AppItem(name: "calendar"),
+        AppItem(name: "camera"),
+        AppItem(name: "clock"),
+        AppItem(name: "applemusic"),
+        AppItem(name: "clips"),
+        AppItem(name: "contacts"),
+        AppItem(name: "iosmessage"),
+        AppItem(name: "mail"),
+        AppItem(name: "photos")
+        ]
+    
+}
