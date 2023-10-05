56ac2942df39599cadf86b67b320ca1bd898d043
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-10-05 10:33:29 +0300

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by Kate Kashko on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
