a3103b3bbfa6a159ad64b6d2607769ee0741d430
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-10-05 10:37:20 +0300

    Create project

---
+++ CustomNavBar/Model/AppItem.swift
@@ -0,0 +1,13 @@
+//
+//  AppItem.swift
+//  CustomNavBar
+//
+//  Created by Kate Kashko on 5.10.2023.
+//
+
+import SwiftUI
+
+
+struct AppItem: Identifiable {
+    
+
---
+++ CustomNavBar/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  CustomNavBar
+//
+//  Created by Kate Kashko on 5.10.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
